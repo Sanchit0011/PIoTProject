d40f6975cf58dede133a55940911ed0ab8b6c875
sanchit <sanchitsh0211@gmail.com>
2019-03-19 11:08:11 +1100

    File which creates config.json

--- createJSON.py
+++ createJSON.py
@@ -2,4 +2,21 @@ import json
 import requests
 
 class createJSON:
+    ranges = {
+        "min-temperature": 20,
+        "max-temperature": 30,
+        "min-humidity": 50,
+        "max-humidity": 60
+        
+        }
+    
+    def create_JSON(self,path,fileName):
+        filePath = path + '/' + fileName + '.json'
+        with open(filePath, "w") as fp:
+            json.dump(self.ranges, fp)
+        
+x = createJSON()
+x.create_JSON("C:/Users/sanch/PIoT_Assignment", "config")
+
+
     
