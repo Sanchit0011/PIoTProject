27f0524e95e505e9d23048d03f12888368df3986
rmit-s3698341-deepika-gill <s3698341@student.rmit.edu.au>
2019-03-20 18:11:03 +1100

    Methods to connect to the database and create the table

--- database.py
+++ database.py
@@ -1,5 +1,19 @@
-# The file contains code to insert the values read to the database
 import sqlite3
+from monitor import monitor
 
 
 class database:
+
+    def create_conn(self):
+        conn = sqlite3.connect('PIoT_db.db')
+        return(conn)
+
+    def create_table(self):
+        conn = self.create_conn()
+        conn.execute('''CREATE TABLE READINGS
+        ( ID INTEGER PRIMARY KEY AUTOINCREMENT,
+          TEMP REAL NOT NULL,
+          HUMIDITY REAL NOT NULL,
+          DATE DATETIME NOT NULL,
+          TIME DATETIME NOT NULL
+        );''')
