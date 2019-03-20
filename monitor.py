71c0ac41bb1c3077a08a2ece6692fe95a1c5bbb9
Deepika Gill <deepika@lingellearning.com>
2019-03-20 15:10:01 +1100

    Added code to read Humidity and Time

--- monitor.py
+++ monitor.py
@@ -4,3 +4,16 @@ from sense_hat import SenseHat
 
 
 class monitor:
+    def getHumidity(self):
+        sense = SenseHat()
+        humidity = sense.get_humidity()
+        return(humidity)
+    
+    def getTime(self):
+        date = datetime.datetime.now()
+        return(date)
+
+
+reading = monitor()
+reading.getHumidity()
+reading.getTime()
