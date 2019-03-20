8985d5aca3f75f2d9b19de32320e512f9a2a8dee
sanchit <sanchitsh0211@gmail.com>
2019-03-20 15:32:41 +1100

    File to get current temp

--- monitor.py
+++ monitor.py
@@ -1,21 +1,22 @@
+# Imported necessary modules
 import os
 from sense_hat import SenseHat
+
+# Defined monitor class
 
 
 class monitor:
 
+    # Function to get cpu temperature
     def get_cpu_temp(self):
         res = os.popen("vcgencmd measure_temp").readline()
         temp = float(res.replace("temp=", "").replace("'C\n", ""))
         return(temp)
 
+    # Function to calculate accurate temperature
     def calc_acc_temp(self):
         sense = SenseHat()
         temp = sense.get_temperature_from_pressure()
         temp_cpu = self.get_cpu_temp()
         temp_acc = temp - ((temp_cpu-temp)/1.5)
         return(temp_acc)
-
-
-reading = monitor()
-reading.calc_acc_temp()
