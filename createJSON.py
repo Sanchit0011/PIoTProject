6a5c42db31e42bdf0a24be6a5d92d349059e5a63
sanchit <sanchitsh0211@gmail.com>
2019-03-21 13:47:53 +1100

    File which creates config.json

--- createJSON.py
+++ createJSON.py
@@ -1,7 +1,14 @@
+# The file contains code to create JSON file
+
+# Imported the necessary module
 import json
+
+# Defined createJSON class
 
 
 class createJSON:
+
+    # Created ranges dictionary
     ranges = {
         "min-temperature": 20,
         "max-temperature": 30,
@@ -10,11 +17,12 @@ class createJSON:
 
         }
 
+    # Function to create JSON file using ranges dictionary
     def create_JSON(self, path, fileName):
         filePath = path + '/' + fileName + '.json'
         with open(filePath, "w") as fp:
             json.dump(self.ranges, fp, indent=4)
 
-
+# Created object to call create_JSON
 x = createJSON()
 x.create_JSON("C:/Users/sanch/PIoT_Assignment", "config")
