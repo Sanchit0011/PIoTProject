9a14d98e1c1d0c3dc6a00013f9f1355b8cc3dace
sanchit <sanchitsh0211@gmail.com>
2019-03-25 22:49:58 +1100

    File which inserts reading data into database

--- database.py
+++ database.py
@@ -1,36 +1,52 @@
+# The file contains code to insert the values read into the database
+
+# Imported the necessary modules
+import os
 import sqlite3
 from monitor import monitor
+from notifyPushbullet import notifyPushbullet
+
+# Defined database class
 
 
 class database:
 
+    # Function to create connection to database
     def create_conn(self):
-        conn = sqlite3.connect('PIoT_db.db')
+        path1 = os.path.realpath(__file__)
+        path2 = os.path.basename(__file__)
+        rel_path = path1.replace(path2, "")
+
+        conn = sqlite3.connect(rel_path+'PIoT_db.db')
         return(conn)
 
+    # Function to create readings table if it does not exist
     def create_table(self):
         conn = self.create_conn()
-        conn.execute('''CREATE TABLE READINGS
+        conn.execute('''CREATE TABLE IF NOT EXISTS READINGS
         ( ID INTEGER PRIMARY KEY AUTOINCREMENT,
           TEMP REAL NOT NULL,
           HUMIDITY REAL NOT NULL,
           DATE DATETIME NOT NULL,
-          TIME DATETIME NOT NULL
-        );''')
+          TIME DATETIME NOT NULL,
+          NOTIFIED INT NOT NULL
+          );''')
 
-    # Function to insert reading data into table
+    # Function to insert data reading data into table
     def insert_table(self):
         conn = self.create_conn()
         mon = monitor()
-        temp = round(mon.calc_acc_temp(), 2)
-        humid = round(mon.getHumidity(), 2)
+        npb = notifyPushbullet()
+        notified = npb.notify()
+        temp = round(npb.temperature, 2)
+        humid = round(npb.humidity, 2)
         date = mon.getDate()
         time = mon.getTime()
-        conn.execute("INSERT INTO READINGS(TEMP, HUMIDITY, DATE, TIME) \
-        VALUES(?, ?, ?, ?);", (temp, humid, date, time))
+
+        conn.execute("INSERT INTO READINGS(TEMP, HUMIDITY, DATE, TIME, NOTIFIED) \
+        VALUES(?, ?, ?, ?, ?);", (temp, humid, date, time, notified))
 
         if((temp > 0 and temp < 100) and (humid > 0 and humid < 100)):
             conn.commit()
-            print("Records created successfully")
 
         conn.close()
