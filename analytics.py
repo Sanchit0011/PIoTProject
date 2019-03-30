bd7e9892ca2db8a88eae6aa693c5113065bc95d3
sanchit <sanchitsh0211@gmail.com>
2019-03-30 16:51:26 +1100

    File to produce visualizations

---
+++ analytics.py
@@ -0,0 +1,15 @@
+# The file contains code to produce visualizations based on readings data
+
+# Imported the necessary modules
+from sshPlot import sshPlot
+import seaborn as sns
+from database import database
+import pandas as pd
+import pygal
+import cairosvg
+from matplotlib.lines import Line2D
+
+# Defined analytics class
+
+
+class analytics:
