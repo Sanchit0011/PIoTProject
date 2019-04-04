0461413a0fb8562d0306e20e06c094c4b8927e07
rmit-s3698341-deepika-gill <s3698341@student.rmit.edu.au>
2019-04-04 15:57:52 +1100

    Barchart Code

--- analytics.py
+++ analytics.py
@@ -13,3 +13,32 @@ from matplotlib.lines import Line2D
 
 
 class analytics:
+
+# Function to create bar chart
+    def create_bar_chart(self):
+
+        # Connected to the database
+        db = database()
+        conn = db.create_conn()
+
+        # Retrieved min, max temp, humidity and data and stored in data frame
+        data = pd.read_sql_query('''SELECT DATE, MIN(TEMP), MAX(TEMP), MIN(HUMIDITY),
+        MAX(HUMIDITY) FROM READINGS GROUP BY DATE''', conn)
+
+        # Created bar chart comparing min, max temp, humidity for each day
+        s_bar = pygal.StackedBar()
+        s_bar.title = 'Min, max temperature and humidity comparison per day'
+        s_bar.x_labels = map(str, data['DATE'])
+        s_bar.add('Min-Temperature', data['MIN(TEMP)'])
+        s_bar.add('Max-Temperature', data['MAX(TEMP)'])
+        s_bar.add('Min-Humidity', data['MIN(HUMIDITY)'])
+        s_bar.add('Max-Humidity', data['MAX(HUMIDITY)'])
+        s_bar.render_to_png('test.png')
+
+# Created object to create visualizations
+viz = analytics()
+viz.create_bar_chart()
+
+
+
+
